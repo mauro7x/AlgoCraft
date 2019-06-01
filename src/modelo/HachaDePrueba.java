4662b00fc6da8aa1dda9abd1efb53ea72b85f5d9
Mauro <mauroparafati@gmail.com>
2019-06-01 02:01:24 -0300

    Agrego archivos JavaFX, creo paquete interfaz y modelo

--- src/modelo/HachaDePrueba.java
+++ src/modelo/HachaDePrueba.java
@@ -1,3 +1,5 @@
+package modelo;
+
 public class HachaDePrueba {
 
     private int durabilidad;
--- test/modelo/HachaDePruebaTest.java
+++ test/modelo/HachaDePruebaTest.java
@@ -1,3 +1,4 @@
+import modelo.HachaDePrueba;
 import org.junit.Test;
 
 import static org.junit.Assert.*;

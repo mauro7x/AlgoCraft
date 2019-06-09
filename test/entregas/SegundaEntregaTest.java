86ec11dd018b9a6bcc60facb8d9486279ec84d11
sankle <santi.klein@hotmail.com>
2019-06-09 12:40:07 -0300

    Pruebas segunda entrega agregadas

--- test/entregas/SegundaEntregaTest.java
+++ test/entregas/SegundaEntregaTest.java
@@ -1,4 +1,81 @@
 package entregas;
 
+import modelo.jugador.Jugador;
+import modelo.objetos.materiales.*;
+import modelo.objetos.herramientas.*;
+import modelo.recursos.*;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class SegundaEntregaTest {
+
+    //Pruebas de Construccion de herramienta
+    @Test
+    public void test01HachaDeMaderaSeConstruyeDeLaFormaCorrecta(){
+
+    }
+
+    @Test
+    public void test02HachaDePiedraSeConstruyeDeLaFormaCorrecta(){
+
+    }
+
+    @Test
+    public void test03HachaDeMetalSeConstruyeDeLaFormaCorrecta(){
+
+    }
+
+    @Test
+    public void test04PicoDeMaderaSeConstruyeDeLaFormaCorrecta(){
+
+    }
+
+    @Test
+    public void test05PicoDePiedraSeConstruyeDeLaFormaCorrecta(){
+
+    }
+
+    @Test
+    public void test06PicoDeMetalSeConstruyeDeLaFormaCorrecta(){
+
+    }
+
+    @Test
+    public void test07PicoFinoSeConstruyeDeLaFormaCorrecta(){
+
+    }
+
+    //Pruebas de Jugador
+    @Test
+    public void test01JugadorSeInicializaDeLaFormaCorrectaEnElMapa(){
+
+    }
+
+    @Test
+    public void test02JugadorPuedeMoverseParaTodasLasDireccionesVacias(){
+
+    }
+
+    //Pruebas de Mapa
+    @Test
+    public void test01NoSePuedeOcuparUnCasilleroVacioDelTerreno(){
+
+    }
+
+    @Test
+    public void test02NoSePuedeOcuparUnCasilleroOcupadoDelTerreno(){
+
+    }
+
+    @Test
+    public void test03MaterialOPersonajePuedenOcuparUnCasilleroVacioDelTerreno(){
+
+    }
+
+    //Pruebas de Juego
+    @Test
+    public void test01SeIniciaElJuegoConElTerrenoInicializadoYElJugadorInicializado(){
+
+    }
 }

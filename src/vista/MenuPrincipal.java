f03e5286b1ee4a672baec202aeacb0b55e289056
Nicolas Aguerre <aguerre31@gmail.com>
2019-06-17 00:16:07 -0300

    Agregada la vista de Menu Principal y Acerca De

---
+++ src/vista/MenuPrincipal.java
@@ -0,0 +1,92 @@
+package vista;
+import controladores.menuPrincipal.BotonAcercaDeHandler;
+import javafx.animation.FadeTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MenuPrincipal extends Application{
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Algocraft");
+
+        Scene escenaMenuPrincipal = generarMenuPrincipal();
+        stage.setScene(escenaMenuPrincipal);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    private Scene generarMenuPrincipal() throws IOException {
+        String estiloBotones = "-fx-opacity: 50; -fx-pref-width: 200px";
+
+        Button botonJugar = new Button("Jugar");
+        Button botonAcercaDe = new Button("Acerca De");
+
+        BotonAcercaDeHandler handlerAcercade = new BotonAcercaDeHandler();
+        botonAcercaDe.setOnAction(handlerAcercade);
+
+        botonJugar.setStyle(estiloBotones);
+        botonAcercaDe.setStyle(estiloBotones);
+
+        Image logo = new Image("media/Algocraft.png");
+        ImageView vistaLogo = new ImageView(logo);
+        VBox contenedorPrincipal = new VBox();
+
+        contenedorPrincipal.getChildren().addAll(vistaLogo,botonJugar,botonAcercaDe);
+        contenedorPrincipal.setSpacing(20);
+        contenedorPrincipal.setAlignment(Pos.CENTER);
+
+        BackgroundImage fondo1= new BackgroundImage(new Image("media/fondo.jpg",900,500,false,true),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+
+        BackgroundImage fondo2= new BackgroundImage(new Image("media/fondo2.jpg",900,500,false,true),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+
+        BackgroundImage fondo3= new BackgroundImage(new Image("media/fondo3.jpg",900,500,false,true),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+
+        BackgroundImage[] fondos = {fondo1,fondo2,fondo3};
+
+        contenedorPrincipal.setBackground(new Background(fondo1));
+        Timeline carruselFondo = new Timeline(new KeyFrame(Duration.seconds(3), new EventHandler<ActionEvent>() {
+            int i = 0;
+
+            @Override
+            public void handle(ActionEvent event) {
+                i = (i+1)%fondos.length;
+                contenedorPrincipal.setBackground(new Background(fondos[i]));
+            }
+        }));
+        carruselFondo.setCycleCount(Timeline.INDEFINITE);
+        carruselFondo.play();
+
+        FadeTransition transicion = new FadeTransition(Duration.millis(3500),vistaLogo);
+        transicion.setFromValue(0);
+        transicion.setToValue(10);
+        transicion.play();
+
+        return new Scene(contenedorPrincipal,900,500);
+    }
+}
